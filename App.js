a29d59f2b7a4fe41d4e62179b19fc48ceac649f2
Taro Matsuzawa <taro@georepublic.co.jp>
2018-06-29 19:11:55 +0900

    ボタンの初期実装

--- App.js
+++ App.js
@@ -4,11 +4,41 @@ import {
   Text,
   View,
   Platform,
+  TouchableOpacity,
 } from 'react-native';
 
 const STATUSBAR_HEIGHT = Platform.OS == 'ios' ? 20 : StatusBar.currentHeight;
 
+// 1: ボタンのFunctional Component
+const CalcButton = (props) => {
+  const flex = props.flex ? props.flex : 1
+  return (
+    <TouchableOpacity
+      style={[styles.calcButton, {flex: flex}]}
+      onPress={() => {props.btnEvent()}}>
+      <Text style={styles.calcButtonText}>{props.label}</Text>
+    </TouchableOpacity>
+  )
+}
+
 export default class App extends React.Component {
+
+  // 2: ボタンの役割ごとに関数を作成
+  valueButton = (value) => {
+  }
+
+  enterButton = () => {
+  }
+
+  calcButton = (value) => {
+  }
+
+  acButton = () => {
+  }
+
+  cButton = () => {
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -22,6 +52,10 @@ export default class App extends React.Component {
         </View>
         <View style={styles.buttons}>
           <View style={styles.buttonsLine}>
+            { /* 3: ボタンを配置 */ }
+            <CalcButton flex={2} label={'AC'} btnEvent={() => this.acButton()} />
+            <CalcButton label={'C'} btnEvent={() => this.cButton()} />
+            <CalcButton label={'+'} btnEvent={() => this.calcButton('+')} />
           </View>
           <View style={styles.buttonsLine}>
           </View>
@@ -81,5 +115,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     borderWidth: 1,
+  },
+  //
+  calcButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
+    borderWidth: 1,
+    borderColor: "#b0c4de",
+  },
+  calcButtonText: {
+    fontSize: 20,
   },
 });
