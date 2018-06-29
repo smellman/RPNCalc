8dcd9ff293e775ec328df21f9397267483c71d0c
Taro Matsuzawa <taro@georepublic.co.jp>
2018-06-29 19:58:34 +0900

    ボタンをすべて定義

--- App.js
+++ App.js
@@ -20,7 +20,6 @@ const CalcButton = (props) => {
   )
 }
 
-// 1: ボタンのFragmentを返すFunctional Component
 const CalcButtons = (props) => {
   return (
     <React.Fragment>
@@ -40,7 +39,7 @@ const CalcButtons = (props) => {
 
 export default class App extends React.Component {
 
-  // 2: ボタンの定義
+  // 1: ボタンの定義
   buttons = [
     [
       {
@@ -57,6 +56,76 @@ export default class App extends React.Component {
         btnEvent: () => {this.calcButton('+')},
       }
     ],
+    [
+      {
+        label: '7',
+        btnEvent: () => {this.valueButton('7')},
+      },
+      {
+        label: '8',
+        btnEvent: () => {this.valueButton('8')},
+      },
+      {
+        label: '9',
+        btnEvent: () => {this.valueButton('9')},
+      },
+      {
+        label: '-',
+        btnEvent: () => {this.calcButton('-')},
+      }
+    ],
+    [
+      {
+        label: '4',
+        btnEvent: () => {this.valueButton('4')},
+      },
+      {
+        label: '5',
+        btnEvent: () => {this.valueButton('5')},
+      },
+      {
+        label: '6',
+        btnEvent: () => {this.valueButton('6')},
+      },
+      {
+        label: '*',
+        btnEvent: () => {this.calcButton('*')},
+      }
+    ],
+    [
+      {
+        label: '1',
+        btnEvent: () => {this.valueButton('1')},
+      },
+      {
+        label: '2',
+        btnEvent: () => {this.valueButton('2')},
+      },
+      {
+        label: '3',
+        btnEvent: () => {this.valueButton('3')},
+      },
+    ],
+    [
+      {
+        label: '0',
+        btnEvent: () => {this.valueButton('0')},
+      },
+      {
+        label: '.',
+        btnEvent: () => {this.valueButton('.')},
+      },
+      {
+        label: '/',
+        btnEvent: () => {this.calcButton('/')},
+      }
+    ],
+    [
+      {
+        label: 'Enter',
+        btnEvent: () => {this.enterButton()}
+      }
+    ]
   ]
 
   valueButton = (value) => {
@@ -86,22 +155,27 @@ export default class App extends React.Component {
           </View>
         </View>
         <View style={styles.buttons}>
+          { /* 2: CalcButtons でぞれぞれのViewに配置をしていく */ }
           <View style={styles.buttonsLine}>
-            { /* 3: CalcButtons を利用 */ }
             <CalcButtons buttons={this.buttons[0]} />
           </View>
           <View style={styles.buttonsLine}>
+            <CalcButtons buttons={this.buttons[1]} />
           </View>
           <View style={styles.buttonsLine}>
+            <CalcButtons buttons={this.buttons[2]} />
           </View>
           <View style={styles.lastButtonLinesContainer}>
             <View style={styles.twoButtonLines}>
               <View style={styles.buttonsLine}>
+                <CalcButtons buttons={this.buttons[3]} />
               </View>
               <View style={styles.buttonsLine}>
+                <CalcButtons buttons={this.buttons[4]} />
               </View>
             </View>
             <View style={styles.enterButtonContainer}>
+              <CalcButtons buttons={this.buttons[5]} />
             </View>
           </View>
         </View>
